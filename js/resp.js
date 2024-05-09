fd1b4c5da86d40cc89dab60eb03ad17a9f9421c0
Ketu Patel <pketu916@gmail.com>
2024-05-09 19:12:29 +0530

    Update resp.js

--- js/resp.js
+++ js/resp.js
@@ -1,13 +1,23 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('.navbar')
-navList = document.querySelector('.nav-list')
-rightNav = document.querySelector('.rightNav')
+document.addEventListener("DOMContentLoaded", function() {
+    const burger = document.querySelector('.burger');
+    const navbar = document.querySelector('.navbar');
+    const navList = document.querySelector('.nav-list');
 
+    // Function to toggle responsive classes
+    function toggleResponsiveClasses() {
+        navList.classList.toggle('v-class-resp');
+        navbar.classList.toggle('h-nav-resp');
+    }
 
+    if (burger) {
+        burger.addEventListener('click', () => {
+            toggleResponsiveClasses();
+        });
+    }
 
-burger.addEventListener('click', () => {
-    rightNav.classList.toggle('v-class-resp');
-    navList.classList.toggle('v-class-resp');
-    navbar.classList.toggle('h-nav-resp');
-
-})
+    // Get all navigation links and add event listeners to toggle classes
+    const navLinks = document.querySelectorAll('.nav-list li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', toggleResponsiveClasses);
+    });
+});
